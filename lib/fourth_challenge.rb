b03a6115b50f7cd4f4fea999deabb9c0542b7db2
Amanda Rolon <abrolon87@gmail.com>
2019-12-17 17:13:24 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,12 +9,12 @@ def fourth_challenge
           :name => "Benvolio",
           :age => "17",
           :attitude => "worried",
-        }
+        },
         {
           :name => "Mercutio",
           :age => "18",
           :attitude => "hot-headed",
-        }#your hashes here!
+        },#your hashes here!
       ]
    },
    :capulet => {
@@ -26,12 +26,12 @@ def fourth_challenge
           :name => "Steven",
           :age => "30",
           :attitude => "confused",
-        }
+        },
         {
           :name => "Nurse",
           :age => "44",
           :attitude => "worried",
-        }#your hashes here!
+        },#your hashes here!
       ]
    }
 }
